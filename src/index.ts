78947685b5d78a0df9b8f7e8af27026f45431040
Alan Simon <alancarlsimon@gmail.com>
2021-04-12 14:49:35 -0400

    Added contents from replit starter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,54 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 
+const PORT = process.env.PORT || 3000
+
+
 const app = express();
-const port = 3000;
-app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
-  res.send('The sedulous ugh hyena ate the antelope!');
-});
-app.listen(port, (err: any) => {
-  if (err) {
-    return console.error(err);
+app.use(bodyParser.json())
+
+app.get('/', handleIndex)
+app.post('/start', handleStart)
+app.post('/move', handleMove)
+app.post('/end', handleEnd)
+
+
+app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
+
+
+function handleIndex(request, response) {
+  var battlesnakeInfo = {
+    apiversion: '1',
+    author: '',
+    color: '#888888',
+    head: 'default',
+    tail: 'default'
   }
-  return console.log(`server is listening on ${port}`);
-});
+  response.status(200).json(battlesnakeInfo)
+}
+
+function handleStart(request, response) {
+  var gameData = request.body
+
+  console.log('START')
+  response.status(200).send('ok')
+}
+
+function handleMove(request, response) {
+  var gameData = request.body
+
+  var possibleMoves = ['up', 'down', 'left', 'right']
+  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+
+  console.log('MOVE: ' + move)
+  response.status(200).send({
+    move: move
+  })
+}
+
+function handleEnd(request, response) {
+  var gameData = request.body
+
+  console.log('END')
+  response.status(200).send('ok')
+}
